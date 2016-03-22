897dd763bdc61ca55da635728f1fa1c9ac72fa07
Mitchell de Rijcke <mitchell@de.rijcke.org>
2016-03-22 11:01:26 +0100

    0.1.0: initial release

---
+++ bin/gui.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const electronPath = require("electron-prebuilt")
+const spawn = require("child_process").spawn
+
+var args = process.argv.slice(2)
+args.unshift(__dirname + "/../")
+
+childProcess.spawn(electronPath, args, { stdio: "inherit" })
